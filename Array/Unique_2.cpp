601070bf05736ef504349b5e8b6d027960d79d5e
Raihan <12mdraihan34@gmail.com>
2026-07-16 14:02:34 +0600

    Refactor Unique_2.cpp to use Bubble Sort

--- Array/Unique_2.cpp
+++ Array/Unique_2.cpp
@@ -1,22 +1,42 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
     int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+    int arr[100];
+    int i, j;
+    int temp;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        scanf("%d", &arr[i]);
     }
-    sort(arr, arr + n);
-    for (int i = 0; i < n; i++)
+
+    /* Bubble Sort */
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for (i = 0; i < n; i++)
     {
         if (i == 0 || arr[i] != arr[i - 1])
         {
-            cout << arr[i] << " ";
+            printf("%d ", arr[i]);
         }
     }
+
+    printf("\n");
+
     return 0;
 }
