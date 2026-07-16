970beaddfcd21b98e77d047cb1ebf43fa00a6860
Raihan <12mdraihan34@gmail.com>
2026-07-16 13:26:30 +0600

    Replace iostream with stdio for input/output

--- Input-Output-and-Condition/Complex_multiplication.cpp
+++ Input-Output-and-Condition/Complex_multiplication.cpp
@@ -1,11 +1,15 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
     long long a, b, c;
-    cin >> a >> b >> c;
-    long long x = a * b;
-    cout << x % c << endl;
+    long long x;
+
+    scanf("%lld %lld %lld", &a, &b, &c);
+
+    x = a * b;
+
+    printf("%lld\n", x % c);
+
     return 0;
 }
