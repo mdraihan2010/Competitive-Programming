181d699ea877464806e79f9868167f8c6ab73ef6
Raihan <12mdraihan34@gmail.com>
2026-07-16 14:00:33 +0600

    Implement bubble sort and switch to C I/O
    Replaced C++ STL sort with bubble sort implementation and changed input/output methods from C++ to C.

--- Array/Sorting.cpp
+++ Array/Sorting.cpp
@@ -1,19 +1,36 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
     int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+    int arr[100];
+    int i, j;
+    int temp;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        scanf("%d", &arr[i]);
     }
-    sort(arr, arr + n);
-    for (int i = 0; i < n; i++)
+
+    for (i = 0; i < n - 1; i++)
     {
-        cout << arr[i] << " ";
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
     return 0;
 }
