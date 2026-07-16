94406dc111b9479ef6657439d8fa91f112a01723
Raihan <12mdraihan34@gmail.com>
2026-07-16 13:38:00 +0600

    Refactor Digit_Sum.cpp to use scanf and printf

--- Loop/Digit_Sum.cpp
+++ Loop/Digit_Sum.cpp
@@ -1,17 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
-    long long n, sum = 0, rem;
-    cin >> n;
-    n = abs(n);
+    long long n;
+    long long sum = 0;
+    long long rem;
+
+    scanf("%lld", &n);
+
+    if (n < 0)
+    {
+        n = -n;
+    }
+
     while (n != 0)
     {
         rem = n % 10;
-        sum += rem;
-        n /= 10;
+        sum = sum + rem;
+        n = n / 10;
     }
-    cout << sum << endl;
+
+    printf("%lld\n", sum);
+
     return 0;
 }
