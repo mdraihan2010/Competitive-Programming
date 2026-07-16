23e415ddcdeb5016e483ce7dadcfca7bb3200d59
Raihan <12mdraihan34@gmail.com>
2026-07-16 13:37:35 +0600

    Convert Decimal to Binary using C Standard I/O
    Replaced C++ input/output with C standard I/O functions.

--- Loop/Decimal_to_binary.cpp
+++ Loop/Decimal_to_binary.cpp
@@ -1,26 +1,28 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
     int n;
-    cin >> n;
-
     int binary[100];
     int i = 0;
+    int j;
+
+    scanf("%d", &n);
 
     while (n > 0)
     {
         binary[i] = n % 2;
         i++;
+
         n = n / 2;
     }
 
-    for (int j = i - 1; j >= 0; j--)
+    for (j = i - 1; j >= 0; j--)
     {
-        cout << binary[j];
+        printf("%d", binary[j]);
     }
 
-    cout << endl;
+    printf("\n");
+
     return 0;
 }
