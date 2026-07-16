fdb02d55a3f93f83ba9831ce2b4dda9d9c876245
Raihan <12mdraihan34@gmail.com>
2026-07-16 13:37:09 +0600

    Refactor Complex Exponentiation to use C standard I/O

--- Loop/Complex_Exponentiation.cpp
+++ Loop/Complex_Exponentiation.cpp
@@ -1,15 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
     int a, b, c;
-    cin >> a >> b >> c;
     int ans = 1;
-    for (int i = 1; i <= b; i++)
+    int i;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    for (i = 1; i <= b; i++)
     {
         ans = (ans * a) % c;
     }
-    cout << ans << endl;
+
+    printf("%d\n", ans);
+
     return 0;
 }
