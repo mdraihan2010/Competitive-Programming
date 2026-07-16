e46e77936875be381249e54814876965c39fc93d
Raihan <12mdraihan34@gmail.com>
2026-07-16 13:51:48 +0600

    Refactor to use scanf/printf and improve logic
    Refactored the program to use scanf and printf instead of cin and cout, and adjusted the logic for calculating maximum difference.

--- Array/Consecutive_differences.cpp
+++ Array/Consecutive_differences.cpp
@@ -1,27 +1,47 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
     int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+    int arr[100];
+    int i;
+    int max_diff;
+    int current_diff;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        scanf("%d", &arr[i]);
     }
 
-    int max_diff = abs(arr[1] - arr[0]);
-
-    for (int i = 1; i < n; i++)
+    if (arr[1] > arr[0])
     {
-        int current_diff = abs(arr[i] - arr[i - 1]);
+        max_diff = arr[1] - arr[0];
+    }
+    else
+    {
+        max_diff = arr[0] - arr[1];
+    }
+
+    for (i = 1; i < n; i++)
+    {
+        if (arr[i] > arr[i - 1])
+        {
+            current_diff = arr[i] - arr[i - 1];
+        }
+        else
+        {
+            current_diff = arr[i - 1] - arr[i];
+        }
+
         if (current_diff > max_diff)
         {
             max_diff = current_diff;
         }
     }
 
-    cout << max_diff << endl;
+    printf("%d\n", max_diff);
+
     return 0;
 }
