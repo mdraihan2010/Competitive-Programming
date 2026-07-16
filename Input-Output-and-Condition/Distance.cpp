2999a895419f6b2ec42cb60a66319ee5fdca23ec
Raihan <12mdraihan34@gmail.com>
2026-07-16 13:27:18 +0600

    Refactor Distance.cpp to use scanf and printf

--- Input-Output-and-Condition/Distance.cpp
+++ Input-Output-and-Condition/Distance.cpp
@@ -1,17 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <math.h>
 
 int main()
 {
     int x1, y1, x2, y2;
-    cin >> x1 >> y1 >> x2 >> y2;
+    double x, y, distance;
 
-    double x = x2 - x1;
-    double y = y2 - y1;
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
 
-    double distance = sqrt(x * x + y * y);
+    x = x2 - x1;
+    y = y2 - y1;
 
-    cout << fixed << setprecision(2) << distance << endl;
+    distance = sqrt(x * x + y * y);
+
+    printf("%.2lf\n", distance);
 
     return 0;
 }
