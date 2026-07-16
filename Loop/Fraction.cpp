2fff4a1fd4114f7ee58aa4e593b05f9dd89f8d7a
Raihan <12mdraihan34@gmail.com>
2026-07-16 13:41:23 +0600

    Refactor GCD calculation using scanf and printf

--- Loop/Fraction.cpp
+++ Loop/Fraction.cpp
@@ -1,10 +1,27 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
-int main() {
-    int a,b;
-    cin >>a>>b;
-    int gcd = __gcd(a, b);
-    cout << a/gcd << " " << b/gcd << endl;
+int main()
+{
+    int a, b;
+    int x, y;
+    int temp;
+    int gcd;
+
+    scanf("%d %d", &a, &b);
+
+    x = a;
+    y = b;
+
+    while (y != 0)
+    {
+        temp = y;
+        y = x % y;
+        x = temp;
+    }
+
+    gcd = x;
+
+    printf("%d %d\n", a / gcd, b / gcd);
+
     return 0;
 }
