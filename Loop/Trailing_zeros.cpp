4a5497e8070669d2a5b545adb0cf80ed2918bea7
Raihan <12mdraihan34@gmail.com>
2026-07-16 13:47:17 +0600

    Refactor Trailing_zeros.cpp to use scanf and printf

--- Loop/Trailing_zeros.cpp
+++ Loop/Trailing_zeros.cpp
@@ -1,18 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
     int n;
-    cin >> n;
     int count = 0;
+
+    scanf("%d", &n);
 
     while (n >= 5)
     {
-        count += n / 5; 
-        n /= 5;         
+        count = count + (n / 5);
+        n = n / 5;
     }
-    cout << count << endl;
+
+    printf("%d\n", count);
 
     return 0;
 }
